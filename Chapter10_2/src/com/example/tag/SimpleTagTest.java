4bc7b99795754adbc89ae70ade3249bfddff2db8
Andrius <andriusjavait@gmail.com>
2017-03-09 15:28:59 +0000

    we created a SimpleTagTest which extends SimpleTagSupport

---
+++ Chapter10_2/src/com/example/tag/SimpleTagTest.java
@@ -0,0 +1,7 @@
+package com.example.tag;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+// For the simple tags we need import, extend this class:
+public class SimpleTagTest extends SimpleTagSupport {
+
+}
